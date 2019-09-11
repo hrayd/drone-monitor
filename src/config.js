e7ddb9e87a4db4ce65f44e67156f4cb694e21770
artyhacker <artyhacker@outlook.com>
2019-09-11 11:40:44 +0800

    feat: 将默认host改为从浏览器地址栏读取

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
+const HOST = '';
+const DEFAULT_HOST = HOST || window.location.hostname;
 // 地图服务
-export const BASEMAP_URL = 'http://127.0.0.1:5000/tms/1.0.0/world-fs/{level}/{col}/{row}.png';
+export const BASEMAP_URL = `http://${DEFAULT_HOST}:5000/tms/1.0.0/world-fs/{level}/{col}/{row}.png`;
 // 本地arcgis api服务配置
-const ESRI_HOST = '127.0.0.1';
 const ESRI_PORT = '8080';
 const ESRI_PATH = '/arcgis_js_api/library/4.12';
 // 无人机模拟websocket
-export const WEBSOCKET_URL = 'ws:/127.0.0.1:5050/broadcast';
+export const WEBSOCKET_URL = `ws:/${DEFAULT_HOST}:5050/broadcast`;
 
 /**
  * ONLINE: 使用arcgis在线api服务
@@ -16,12 +17,12 @@ const NET_MODE = 'LOCAL'; // 'ONLINE' or 'LOCAL'
 export const cssUrl = (
   NET_MODE === 'ONLINE'
     ? 'https://js.arcgis.com/3.28/esri/css/esri.css'
-    : `http://${ESRI_HOST}:${ESRI_PORT}${ESRI_PATH}/esri/css/main.css`
+    : `http://${DEFAULT_HOST}:${ESRI_PORT}${ESRI_PATH}/esri/css/main.css`
 );
 
 export const getOptions = () => {
   if (NET_MODE === 'ONLINE') {
     return {};
   }
-  return { url: `http://${ESRI_HOST}:${ESRI_PORT}${ESRI_PATH}/dojo/dojo.js` };
+  return { url: `http://${DEFAULT_HOST}:${ESRI_PORT}${ESRI_PATH}/dojo/dojo.js` };
 };
