949dcd9e35385df23108a90eaaaa36c98c301ff1
artyhacker <artyhacker@outlook.com>
2019-12-03 08:16:26 +0800

    feat: 更新配置

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 const HOST = '';
 const DEFAULT_HOST = HOST || window.location.hostname;
 // 地图服务
-export const BASEMAP_URL = `http://${DEFAULT_HOST}:5000/tms/1.0.0/world-fs/{level}/{col}/{row}.png`;
+// offline: http://${DEFAULT_HOST}:5000/tms/1.0.0/world-fs/{level}/{col}/{row}.png
+// online: https://a.tile.openstreetmap.org/{level}/{col}/{row}.png
+export const BASEMAP_URL = `https://a.tile.openstreetmap.org/{level}/{col}/{row}.png`;
 // 本地arcgis api服务配置
 const ESRI_PORT = '8080';
 const ESRI_PATH = '/arcgis_js_api/library/4.12';
@@ -12,7 +14,7 @@ export const WEBSOCKET_URL = `ws:/${DEFAULT_HOST}:5050/broadcast`;
  * ONLINE: 使用arcgis在线api服务
  * LOCAL: 使用本地搭建的arcgis api服务
  */
-const NET_MODE = 'LOCAL'; // 'ONLINE' or 'LOCAL'
+const NET_MODE = 'ONLINE'; // 'ONLINE' or 'OFFLINE'
 
 export const cssUrl = (
   NET_MODE === 'ONLINE'
